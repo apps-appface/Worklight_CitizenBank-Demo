dbdb06c1b97335df80ad8a3c4db2dcb1fee72706
Rahul249 <rahul@Rahul.Home>
2015-02-20 11:21:46 +0530

    WorkCover NSW Removed

--- Demo/WorkCover NSW/WorkCover/WorkCoverApplication/adapters/GetWorkersListAdapter/GetWorkersListAdapter-impl.js
+++
@@ -1,8 +0,0 @@
-var procedure1Statement = WL.Server
-		.createSQLStatement("select * from WORKER_DETAIL");
-function getWorkersList() {
-	return WL.Server.invokeSQLStatement({
-		preparedStatement : procedure1Statement,
-		parameters : []
-	});
-}
--- Demo/WorkCover NSW/WorkCover/WorkCoverApplication/apps/WorkCover/android/native/assets/www/default/worklight/checksum.js
+++
@@ -1,2 +0,0 @@
-var WL_CHECKSUM = {"checksum":3803096477,"date":1424353231609,"machine":"Shishir-3.Home"};
-/* Date: Thu Feb 19 19:10:31 GMT+05:30 2015 */
